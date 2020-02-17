5ea4ba6097183f78fe05603aa840574a30306b5f
IkerMonzon <IkerMonzon@DESKTOP-10P8SV7>
2020-02-17 08:59:14 +0100

    modHabita

---
+++ src/trivago/ModeloHabitacion.java
@@ -0,0 +1,10 @@
+package trivago;
+
+public class ModeloHabitacion extends Conexion {
+
+	public ModeloHabitacion(String bbdd, String maquina, String usuario, String contrasenia) {
+		super(bbdd, maquina, usuario, contrasenia);
+		// TODO Auto-generated constructor stub
+	}
+
+}
